7dc46520e4927edb9db8ee9fcbded15a98d6e4f1
dwidge <dwidge+gh@gmail.com>
2023-07-08 14:56:04 +0200

    move makeTileLayers buildTerrain

--- src/platformer/buildLevel.ts
+++ src/platformer/buildLevel.ts
@@ -1,17 +1,12 @@
 import {
-  clamp,
   rand,
-  randInt,
   randColor,
   vec2,
   Color,
-  glEnable,
   tileCollisionSize,
   initTileCollision,
   setTileCollisionData,
   tileCollisionRaycast,
-  TileLayerData,
-  TileLayer,
   frameRate,
   engineObjectsUpdate,
   engineObjectsDestroy,
@@ -19,114 +14,18 @@ import {
   setTileCollision,
 } from "../../lib/little";
 
-import { Crate } from "./Crate";
-import { Enemy } from "./Enemy";
 import {
   initSky,
   initParallaxLayers,
   decorateBackgroundTile,
   decorateTile,
   level,
-  tileType_empty,
-  tileType_solid,
-  tileType_ladder,
   getTileBackgroundData,
   tileBackground,
 } from ".";
 import { Player } from "./player";
-import { setTile, getTile } from "./setTile";
-
-function buildTerrain(levelSize: Vector2) {
-  const tileBackground: number[] = [];
-  const tileCollision: number[] = [];
-  let startGroundLevel = 40;
-  let groundLevel = startGroundLevel;
-  let groundSlope = rand(-1, 1);
-  let backgroundDelta = 0,
-    backgroundDeltaSlope = 0;
-  for (let x = 0; x < levelSize.x; x++) {
-    // pull slope towards start ground level
-    groundLevel += groundSlope =
-      rand() < 0.05
-        ? rand(-1, 1)
-        : groundSlope + (startGroundLevel - groundLevel) / 1000;
-
-    // small jump
-    if (rand() < 0.04) groundLevel += rand(9, -9);
-
-    if (rand() < 0.03) {
-      // big jump
-      const jumpDelta = rand(19, -19);
-      startGroundLevel = clamp(startGroundLevel + jumpDelta, 20, 80);
-      groundLevel += jumpDelta;
-      groundSlope = rand(-1, 1);
-    }
-
-    backgroundDelta += backgroundDeltaSlope;
-    if (rand() < 0.1) backgroundDelta = rand(3, -1);
-    if (rand() < 0.1) backgroundDelta = 0;
-    if (rand() < 0.1) backgroundDeltaSlope = rand(-1, 1);
-    backgroundDelta = clamp(backgroundDelta, -1, 3);
-
-    groundLevel = clamp(groundLevel, 20, levelSize.y - 20);
-    for (let y = 0; y < levelSize.y; y++) {
-      const pos = vec2(x, y);
-
-      let frontTile = tileType_empty;
-      if (y < groundLevel) frontTile = tileType_solid;
-
-      let backTile = tileType_empty;
-      if (y < groundLevel + backgroundDelta) backTile = tileType_solid;
-
-      setTile(levelSize, tileCollision, pos, frontTile);
-      setTile(levelSize, tileBackground, pos, backTile);
-    }
-  }
-
-  // add random holes
-  for (let i = levelSize.x; i--; ) {
-    const pos = vec2(rand(levelSize.x), rand(levelSize.y / 2, 9));
-    const height = randInt(19, 1);
-    const offset = vec2();
-    for (offset.x = randInt(19, 1); --offset.x; )
-      for (offset.y = height; --offset.y; )
-        setTile(levelSize, tileCollision, pos.add(offset), tileType_empty);
-  }
-
-  // add ladders
-  for (let ladderCount = 40; ladderCount--; ) {
-    // pick random pos
-    const pos = vec2(randInt(levelSize.x), randInt(levelSize.y));
-
-    // find good place to put ladders
-    let state = 0,
-      ladderTop = 0;
-    for (; pos.y > 9; --pos.y) {
-      const data = getTile(levelSize, tileCollision, pos);
-      if (state == 0 || state == 2)
-        data || state++; // found empty, go to next state
-      else if (state == 1) {
-        data && state++; // found solid, go to next state
-        ladderTop = pos.y;
-      } else if (state == 3 && data) {
-        // found solid again, build ladder
-        for (; ++pos.y <= ladderTop; )
-          setTile(levelSize, tileCollision, pos, tileType_ladder);
-        break;
-      }
-    }
-  }
-
-  // spawn crates
-  for (let crateCount = 100; crateCount--; )
-    new Crate(vec2(randInt(levelSize.x) + 0.5, randInt(levelSize.y)));
-
-  // spawn enemies
-  for (let enemyCount = 10; enemyCount--; )
-    new Enemy(vec2(rand(levelSize.x), rand(levelSize.y)));
-
-  return { tileBackground, tileCollision };
-}
+import { buildTerrain } from "./buildTerrain";
+import { makeTileLayers } from "./makeTileLayers";
 
 function generateLevel(levelSize: Vector2) {
   // clear old level
@@ -140,60 +39,6 @@ function calcPlayerStart(levelSize: Vector2) {
     vec2(playerStartPos.x, 0)
   );
   return raycastHit.add(vec2(0, 1));
-}
-
-function makeTileLayers(
-  levelSize: Vector2,
-  tileBackground: number[],
-  tileCollision: number[],
-  levelColor: Color
-) {
-  // create tile layers
-  const tileLayer = new TileLayer(vec2(), levelSize);
-  const tileBackgroundLayer = new TileLayer(vec2(), levelSize);
-  tileLayer.renderOrder = -1000;
-  tileBackgroundLayer.renderOrder = -2000;
-
-  const pos = vec2();
-  for (pos.x = levelSize.x; pos.x--; )
-    for (pos.y = levelSize.y; pos.y--; ) {
-      // foreground tiles
-      let tileType = getTile(levelSize, tileBackground, pos);
-      if (tileType) {
-        let direction, mirror, tileIndex, color;
-        if (tileType == tileType_solid) {
-          tileIndex = (5 + rand() ** 3 * 2) | 0;
-          color = levelColor.mutate(0.03);
-          direction = randInt(4);
-          mirror = randInt(2);
-        } else if (tileType == tileType_ladder) tileIndex = 7;
-
-        const data = new TileLayerData(tileIndex, direction, mirror, color);
-        tileLayer.setData(pos, data);
-      }
-
-      // background tiles
-      tileType = getTile(levelSize, tileCollision, pos);
-      if (tileType) {
-        const data = new TileLayerData(
-          (5 + rand() ** 3 * 2) | 0,
-          randInt(4),
-          randInt(2),
-          levelColor.mutate().scale(0.4, 1)
-        );
-        tileBackgroundLayer.setData(pos, data);
-      }
-    }
-  tileLayer.redraw();
-  tileBackgroundLayer.redraw();
-
-  if (!glEnable) {
-    // get rid of background if webgl is not enabled
-    tileBackgroundLayer.destroy();
-    level.tileBackgroundLayer = 0;
-  }
-
-  return { tileLayer, tileBackgroundLayer };
 }
 
 export default function buildLevel() {
--- src/platformer/setTile.ts
+++ src/platformer/setTile.ts
@@ -4,6 +4,7 @@ export const getTile = (levelSize: Vector2, tiles: number[], pos: Vector2) =>
   pos.arrayCheck(levelSize)
     ? tiles[((pos.y | 0) * levelSize.x + pos.x) | 0]
     : 0;
+
 export const setTile = (
   levelSize: Vector2,
   tiles: number[],
