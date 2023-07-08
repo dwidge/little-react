d2fa7337e6e816219b375fcfc572c1f7c5751c61
dwidge <dwidge+gh@gmail.com>
2023-07-08 16:47:37 +0200

    update buildTerrain

--- src/platformer/buildTerrain.ts
+++ src/platformer/buildTerrain.ts
@@ -7,23 +7,36 @@ import { setTile, getTile } from "./setTile";
 export function buildTerrain(levelSize: Vector2) {
   const tileBackground: number[] = [];
   const tileCollision: number[] = [];
+
+  generateGroundLevel(levelSize, tileBackground, tileCollision);
+  generateRandomHoles(levelSize, tileCollision);
+  generateLadders(levelSize, tileCollision);
+  spawnCrates(levelSize);
+  spawnEnemies(levelSize);
+
+  return { tileBackground, tileCollision };
+}
+
+function generateGroundLevel(
+  levelSize: Vector2,
+  tileBackground: number[],
+  tileCollision: number[]
+) {
   let startGroundLevel = 40;
   let groundLevel = startGroundLevel;
   let groundSlope = rand(-1, 1);
   let backgroundDelta = 0,
     backgroundDeltaSlope = 0;
+
   for (let x = 0; x < levelSize.x; x++) {
-    // pull slope towards start ground level
     groundLevel += groundSlope =
       rand() < 0.05
         ? rand(-1, 1)
         : groundSlope + (startGroundLevel - groundLevel) / 1000;
 
-    // small jump
     if (rand() < 0.04) groundLevel += rand(9, -9);
 
     if (rand() < 0.03) {
-      // big jump
       const jumpDelta = rand(19, -19);
       startGroundLevel = clamp(startGroundLevel + jumpDelta, 20, 80);
       groundLevel += jumpDelta;
@@ -37,6 +50,7 @@ export function buildTerrain(levelSize: Vector2) {
     backgroundDelta = clamp(backgroundDelta, -1, 3);
 
     groundLevel = clamp(groundLevel, 20, levelSize.y - 20);
+
     for (let y = 0; y < levelSize.y; y++) {
       const pos = vec2(x, y);
 
@@ -50,8 +64,9 @@ export function buildTerrain(levelSize: Vector2) {
       setTile(levelSize, tileBackground, pos, backTile);
     }
   }
+}
 
-  // add random holes
+function generateRandomHoles(levelSize: Vector2, tileCollision: number[]) {
   for (let i = levelSize.x; i--; ) {
     const pos = vec2(rand(levelSize.x), rand(levelSize.y / 2, 9));
     const height = randInt(19, 1);
@@ -60,38 +75,34 @@ export function buildTerrain(levelSize: Vector2) {
       for (offset.y = height; --offset.y; )
         setTile(levelSize, tileCollision, pos.add(offset), tileType_empty);
   }
+}
 
-  // add ladders
+function generateLadders(levelSize: Vector2, tileCollision: number[]) {
   for (let ladderCount = 40; ladderCount--; ) {
-    // pick random pos
     const pos = vec2(randInt(levelSize.x), randInt(levelSize.y));
-
-    // find good place to put ladders
     let state = 0,
       ladderTop = 0;
     for (; pos.y > 9; --pos.y) {
       const data = getTile(levelSize, tileCollision, pos);
-      if (state == 0 || state == 2)
-        data || state++; // found empty, go to next state
+      if (state == 0 || state == 2) data || state++;
       else if (state == 1) {
-        data && state++; // found solid, go to next state
+        data && state++;
         ladderTop = pos.y;
       } else if (state == 3 && data) {
-        // found solid again, build ladder
         for (; ++pos.y <= ladderTop; )
           setTile(levelSize, tileCollision, pos, tileType_ladder);
         break;
       }
     }
   }
+}
 
-  // spawn crates
+function spawnCrates(levelSize: Vector2) {
   for (let crateCount = 100; crateCount--; )
     new Crate(vec2(randInt(levelSize.x) + 0.5, randInt(levelSize.y)));
+}
 
-  // spawn enemies
+function spawnEnemies(levelSize: Vector2) {
   for (let enemyCount = 10; enemyCount--; )
     new Enemy(vec2(rand(levelSize.x), rand(levelSize.y)));
-
-  return { tileBackground, tileCollision };
 }
